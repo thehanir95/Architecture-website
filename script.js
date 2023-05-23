11233ad7c0dd5afb2487e8c045e4686353f99453
thehanir95 <thehanicis95@gmail.com>
2023-05-23 11:45:49 +0530

    changes

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-document.querySelector('.hamburger-menu').addEventListener('click',()=>{
-document.querySelector('.container').classList.toggle("change");
-
-})
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_usoqsz5";
+    const templateID = "template_lkoegzy";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+      })
+      .catch(err=>console.log(err));
+  
+  }
+  
